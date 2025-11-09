86c9df3a3e12bb00a31a041d76080a1c03a00400
c0ndii <105825543+c0ndii@users.noreply.github.com>
2025-11-09 17:38:39 +0100

    feat: fix regex

--- Models/Validators/UserInputValidator.cs
+++ Models/Validators/UserInputValidator.cs
@@ -8,10 +8,10 @@ namespace UserNotepad.Models.Validators
         {
             RuleFor(x => x.Name).NotEmpty().WithMessage("Name is required!")
                 .MaximumLength(50).WithMessage("Name must not exceed length of 50!")
-                .Matches(@"^[A-Za-z]+$").WithMessage("Name can contain only letters!");
+                .Matches(@"^[\p{L}]+$").WithMessage("Name can contain only letters!");
             RuleFor(x => x.Surname).NotEmpty().WithMessage("Surname is required!")
                 .MaximumLength(150).WithMessage("Surname must not exceed length of 150!")
-                .Matches(@"^[A-Za-z]+$").WithMessage("Surname can contain only letters!");
+                .Matches(@"^[\p{L}]+$").WithMessage("Surname can contain only letters!");
             RuleFor(x => x.BirthDate).NotEmpty().WithMessage("Birth date is required!")
                 .LessThan(DateTime.Now).WithMessage("Birth date can not be set in future!");
             RuleFor(x => x.Attributes).Must(HaveUniqueKeys).WithMessage("Key value of attribute can not be duplicated!");
